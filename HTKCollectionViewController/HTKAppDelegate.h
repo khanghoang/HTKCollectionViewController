80ba1c2958738787bcc05550b318b50d08b2956b
Triệu Khang <hoangtrieukhang@gmail.com>
2013-10-16 09:58:07 +0700

    Initial Commit

---
+++ HTKCollectionViewController/HTKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HTKAppDelegate.h
+//  HTKCollectionViewController
+//
+//  Created by Triệu Khang on 16/10/13.
+//  Copyright (c) 2013 Triệu Khang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HTKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HTKCollectionViewController/HTKViewController.h
@@ -0,0 +1,13 @@
+//
+//  HTKViewController.h
+//  HTKCollectionViewController
+//
+//  Created by Triệu Khang on 16/10/13.
+//  Copyright (c) 2013 Triệu Khang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HTKViewController : UIViewController
+
+@end
