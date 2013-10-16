6383e27f832ed508cc7440a6e62b434e3cf81540
Triệu Khang <hoangtrieukhang@gmail.com>
2013-10-16 10:52:18 +0700

    Add layout files

---
+++ HTKCollectionViewController/HTKCollectionViewLayout.h
@@ -0,0 +1,13 @@
+//
+//  HTKCollectionViewLayout.h
+//  HTKCollectionViewController
+//
+//  Created by Triệu Khang on 16/10/13.
+//  Copyright (c) 2013 Triệu Khang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HTKCollectionViewLayout : UICollectionViewLayout
+
+@end
---
+++ HTKCollectionViewController/HTKCollectionViewLayout.m
@@ -0,0 +1,13 @@
+//
+//  HTKCollectionViewLayout.m
+//  HTKCollectionViewController
+//
+//  Created by Triệu Khang on 16/10/13.
+//  Copyright (c) 2013 Triệu Khang. All rights reserved.
+//
+
+#import "HTKCollectionViewLayout.h"
+
+@implementation HTKCollectionViewLayout
+
+@end
